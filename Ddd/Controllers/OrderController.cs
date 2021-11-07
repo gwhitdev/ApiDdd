ccf1839ec3f619aef9f9258b8c46a9f82f9419f6
Gareth Whitley <me@garethwhitley.online>
2021-11-07 21:34:38 +0000

    Init

---
+++ Ddd/Controllers/OrderController.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Ddd.Infrastructure.Repositories;
+using Ddd.Core.Domain;
+using Ddd.Core.Interfaces;
+using Ddd.Services.Orders;
+using Microsoft.Extensions.Logging;
+using Ddd.DTOs.Orders;
+
+namespace Ddd.Controllers
+{
+    
+
+    [Route("api/orders")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly OrderService _orderService;
+        private readonly ILogger<OrderController> _logger;
+
+        public OrderController(OrderService orderService, ILogger<OrderController> logger)
+        {
+            _orderService = orderService;
+            _logger = logger;
+        }
+        [Route("/api/orders/add")]
+        [HttpPost]
+        public async Task<ActionResult<AddOrderResponse>> Add([FromBody] AddOrderRequest request)
+        {
+            _logger.LogInformation($"ADD NEW ORDER: {request}");
+            var order = await _orderService.AddNewAsync(request);
+            return Created("/order/add", order);
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get([FromQuery]GetOrderRequest id)
+        {
+            _logger.LogInformation($"SEARCH ID: {id.Search}");
+            var order = await _orderService.GetOrderAsync(id);
+            return Ok(order);
+        }
+        
+    }
+}
---
+++ Ddd/Startup.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Ddd.Extensions;
+using Newtonsoft.Json;
+
+namespace Ddd
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddDatabase(Configuration);
+            services.AddUnitOfWork();
+            services.AddRepositories();
+            services.AddBusinessServices();
+            services.AddCors(c =>
+            {
+                c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin());
+            });
+
+            services.AddSwaggerGen();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseCors(options => options.AllowAnyOrigin());
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+    }
+}
